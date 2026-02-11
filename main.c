424c03e77ca34e90b777e4ab9ed56df119cac700
ArmageddonSR <151889707+ArmageddonSR@users.noreply.github.com>
2026-02-11 08:25:29 +0000

    aaaa

--- main.c
+++ main.c
@@ -2,32 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Node.h"
-
 #include "Queue.h"
 
 int main(int argc , char **argv) {
-  //NodePtr headPtr=NULL;
-  //NodePtr tailPtr=NULL;
-//For struct Queue 
+    Queue q;
+    q.headPtr = NULL;
+    q.tailPtr = NULL;
+    q.size = 0;
 
-  Queue q;
-  q.headPtr=NULL;
-  q.tailPtr=NULL;
-  q.size=0;
-  int i,x;
-   
+    int i, x;
 
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-          //x=dequeue(&headPtr,&tailPtr);
-          x=dequeue_struct(&q);
-        printf("dequeing %d\n",x);
+    for(i = 1; i < argc; i++) {
+        if(strcmp(argv[i], "x") == 0) {
+            // เช็คก่อนว่ามีของในคิวไหม
+            if(q.size > 0) { 
+                x = dequeue_struct(&q);
+                printf("dequeing %d\n", x); 
+            } else {
+                printf("Empty queue\n");
+            }
+        } 
+        else {
+            enqueue_struct(&q, atoi(argv[i]));
         }
-        else 
-        {
-          //enqueue(&headPtr,&tailPtr, atoi(argv[i]));
-          enqueue_struct(&q, atoi(argv[i]));
-        }
- }
-  return 0;
+    }
+    return 0;
 }
